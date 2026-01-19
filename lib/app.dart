bb56497c2149ec4f360a9d9eba3a6cd1b5d4cb73
Jahid01234 <mdjahidhasan790523@gmail.com>
2026-01-19 21:16:47 +0600

    live streaming  setup...

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'Live Streaming App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white
       ),
