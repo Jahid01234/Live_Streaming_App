4536e53846a7b73a90a2192faa30072867ec0ab8
Jahid01234 <mdjahidhasan790523@gmail.com>
2026-01-24 11:51:45 +0600

    - change...........

--- lib/core/const/secret.dart
+++ lib/core/const/secret.dart
@@ -1,5 +1,4 @@
-// final int appID = 1111111; // your zego app id here
-// final String appSign = " "; // your zego app sign here
+final int appID = 1111111; // your zego app id here
+final String appSign = " "; // your zego app sign here
 
-final int appID = 198933688;
-final String appSign = "334dd047bc7c94c78432a21f81145c8bbaaa022b0ba4f0bdb22fe38ff441e88b";
+
